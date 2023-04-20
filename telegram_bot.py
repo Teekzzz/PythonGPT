fb98ceff52aefd59f1265ceb18e5616b90a8d7a6
Teekzzz <124139969+Teekzzz@users.noreply.github.com>
2023-04-20 14:53:32 +0000

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,14 @@
-# Set up the Telegram bot
-#bot = telegram.Bot('6088146021:AAHzfrDc53rGBsc9HOzNP0gxNUvzTmdSzEM')
+#Firstly, looking into cleaning up code by removing any unused or commented out lines
 
-# Set up the OpenAI API
-#openai.api_key = ('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')
 import telegram
 import openai
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters , CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Set up the Telegram bot
 bot = telegram.Bot('6088146021:AAHzfrDc53rGBsc9HOzNP0gxNUvzTmdSzEM')
 
 # Set up the OpenAI API
-openai.api_key = ('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')
+openai.api_key = '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'
 
 # Define a function to generate responses using ChatGPT
 def generate_response(text):
@@ -30,13 +27,10 @@ def start(update, context):
 
 def echo(update, context):
     user_input = update.message.text
-    response = "I'm sorry, I didn't understand. Can you please rephrase?"
-    # Use OpenAI API to generate response
     response = generate_response(user_input)
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 def main():
-    # Replace 'YOUR_TOKEN_HERE' with your bot token
     updater = Updater('6088146021:AAHzfrDc53rGBsc9HOzNP0gxNUvzTmdSzEM', use_context=True)
     dispatcher = updater.dispatcher
 
@@ -44,12 +38,9 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+#Keep your own API Keys & tokens secure - don't hardcode them into your scripts; use env variables or a configuration file.
+#As an example the 'os' module to read the keys from enviroment variables
 
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+import os
+bot.token = os.environ['TELEGRAM_BOT_TOKEN']
+openai_api_key = os.environ['OPENAI_API_KEY']
